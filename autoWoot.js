75157b18f12a4828583d28075f0740710c082109
Chris Hall <chall@pangeaequity.com>
2013-01-15 23:42:12 -0600

    Added rave mode to auto script

--- autoWoot.js
+++ autoWoot.js
@@ -4,8 +4,36 @@
       me              = API.getSelf(),
       onWaitList      = false,
       onDeck          = false,
-      waitListSize    = API.getWaitList().length;
-      
+      waitListSize    = API.getWaitList().length,
+      raveInt         = 0,
+      avatars         = [],
+      currentAvi      = 0;
+
+  (function() {
+    var el = $("#avatar-panel");
+    el.find(".avatar-button").each(function() {
+      var me = $(this);
+      if(!me.find(".avatar-lock-button").size()) {
+        var fileArry = me.find("img").attr("src").split(".")[0].split("/");
+        avatars.push(fileArry[fileArry.length-1]);
+      }
+    });
+  })();
+
+  function activateRaveMode() {
+    raveInt = setInterval(function() {
+      Models.user.changeAvatar(avatars[currentAvi]);
+      currentAvi++;
+      if(currentAvi >= avatars.length) {
+        currentAvi = 0;
+      }
+    }, 500);
+  }
+
+  function deactivateRaveMode() {
+    clearInterval(raveInt);
+  }
+
   var __waitListJoin = API.waitListJoin;
   API.waitListJoin = function() {
     if(djButton.is(":visible")) {
@@ -18,13 +46,13 @@
       onDeck = false;
     }
   }
-  
+
   API.addEventListener(API.DJ_ADVANCE, function(data) {
     if(data.dj.id != me.id) {
       $("#button-vote-positive").click();
     }
   });
-  
+
   API.addEventListener(API.DJ_UPDATE, function(djs) {
     if(onDeck && !onWaitList) {
       //maybe we left?
@@ -38,7 +66,7 @@
     }
     API.waitListJoin();
   });
-  
+
   API.addEventListener(API.WAIT_LIST_UPDATE, function(users) {
     if(onWaitList && users.length < waitListSize && !onDeck) {
       //Make sure we weren't just removed
@@ -51,7 +79,17 @@
         }
       });
     }
-    
+
     API.waitListJoin();
+  });
+
+  API.addEventListener(API.CHAT, function(data) {
+    if(data.message.match(/^##/)) {
+      if(data.message.match("rave$")) {
+        activateRaveMode();
+      } else if(data.message.match("chill$")) {
+        deactivateRaveMode();
+      }
+    }
   });
 })();
